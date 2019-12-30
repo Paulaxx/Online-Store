73dbd3d7083f7df2485dde96a6d8dbc305a5b8e5
Paula <palinkagr@gmail.com>
2019-12-30 17:39:51 +0100

    change database connection

--- OnlineStore/src/main/java/de/vogella/mysql/first/MysqlCon.java
+++ OnlineStore/src/main/java/de/vogella/mysql/first/MysqlCon.java
@@ -24,7 +24,7 @@ class MysqlCon{
 		ResultSet rs=stmt.executeQuery("select * from users");  
 		while(rs.next())  
 			System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
-		con.close();
+		//con.close();
     }
     
     
